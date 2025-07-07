dbb6ccab6415863d1ecf3c255ee035694bac48ef
tesheng <jqwertyu899800@gmail.com>
2025-07-07 22:59:32 +0800

    Add the list of heroes
    建立英雄的清單列表，並且使用者可以選擇英雄並且獲得英雄的詳細資訊

--- src/app/heroes/heroes.ts
+++ src/app/heroes/heroes.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../hero';
+
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -14,4 +17,11 @@ export class Heroes {
     id: 1,
     name: 'Widstorm',
   };
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
